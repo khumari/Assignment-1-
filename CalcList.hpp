42d3f5416e3fb5b700128e7fe3ff0182ab379360
khumari_barakzai <khumaribk@gmail.com>
2021-09-17 09:50:29 -0400

    inherit CalcListInterface class

--- CalcList.hpp
+++ CalcList.hpp
@@ -1,9 +1,12 @@
 #ifndef CALC_LIST_H
 #define CALC_LIST_H
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include "CalcListInterface.hpp"
 
-
+class CalcList : public CalcListInterface
+{
+};
 
 #endif
