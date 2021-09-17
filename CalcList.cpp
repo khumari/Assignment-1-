b0e80636ba37bbb4a242dc8915f064d4f93d91b8
khumari_barakzai <khumaribk@gmail.com>
2021-09-17 13:35:57 -0400

    feat empty basic function definitions

--- CalcList.cpp
+++ CalcList.cpp
@@ -1,1 +1,19 @@
+#include "CalcList.hpp"
 
+double total() const
+{
+    return 0.0;
+}
+
+void newOperation(const FUNCTIONS func, const double operand)
+{
+}
+
+void removeLastOperation()
+{
+}
+std::string toString(unsigned short precision) const
+{
+
+    return "";
+}
--- CalcList.hpp
+++ CalcList.hpp
@@ -7,6 +7,20 @@
 
 class CalcList : public CalcListInterface
 {
+private:
+public:
+    //constructor
+    CalcList()
+    {
+        //
+    }
+
+    double total() const = 0;
+    void newOperation(const FUNCTIONS func, const double operand) = 0;
+    void removeLastOperation() = 0;
+    std::string toString(unsigned short precision) const = 0;
+
+    //Destructor , should deallocate all memeory from linked
 };
 
 #endif
